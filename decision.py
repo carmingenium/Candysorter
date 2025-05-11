d722c80a9fd21d9c5aa71ec33b20fa751b3f8512
Dağhan Sever <daghansever@gmail.com>
2025-05-11 17:50:29 +0300

    Update decision.py

--- decision.py
+++ decision.py
@@ -1,34 +1,21 @@
-# 4 holes: input, store, camera, output
-# from input we get objects in queue, continiously.
-# we rotate the machine 90 degrees so object in the input hole goes to store hole. we get another input at that moment.
-# from store rotate another 90 degrees so the stored object goes to camera hole, input goes to store, from queue we get another input.
-# at this point, camera detects the object in camera hole and selects the right output hole
-# we rotate the machine 90 degrees so the object in camera hole goes to output hole and all the other objects take another step.
-
-# counter clockwise 90 degrees.
-# after every decision we go to default hole for the output hole.
-
 from ultralytics import YOLO
 import cv2
-import time # can be used later for timing the rotation of the machine
 import serial
 
-# Connect to Arduino (adjust the port and baudrate to match your setup)
-arduino = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=.1)
 
+arduino = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=.1)
 # load model and camera
 model = YOLO('model.pt')
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(1) # Open the camera, arg might change depending on the system.
 
 def transmitDetection(data):
   arduino.write(f'{data}\n'.encode()) # Send data to Arduino
-  
+
 def detectAndClassify():
   ret, frame = cap.read()
   if not ret:
     return "error" # goes to default if loop continues
   results = model(frame)
-  
   detected_labels = []
   for box in results[0].boxes:
     cls_id = int(box.cls[0])  # class index
@@ -37,15 +24,14 @@ def detectAndClassify():
     detected_labels.append(label,confidence)
 
   if cv2.waitKey(1) & 0xFF == ord('q'):
-    return "exit" # goes to default if loop continues
-  
+    return "exit"
   # all detected objects are on the list.
   for labels in detected_labels:
     if labels[1] > 0.80:   # if there is any with confidence over 80%, return it.
       return labels[0]
-  return "default"  # else return default
- 
-def color_to_bin(color): 
+  return "default"
+
+def color_to_bin(color): # There is no need to add another bin for default, as any input outside the list will default to 0 bin.
   return {
     "red": 1,
     "blue": 2,
@@ -54,18 +40,15 @@ def color_to_bin(color):
     "brown": 5,
     "green": 6
   }.get(color, 0)
-  
-# Main loop to simulate operation
+
 while True:
 
   color = detectAndClassify()
   if color == "error":
     print("Error reading color")
     continue
-  
   output_bin = color_to_bin(color)
-  transmitDetection(output_bin)  # Send the color to Arduino LCD should be handled in the Arduino code also.
-  
+  transmitDetection(output_bin)  # LCD is handled from this data in Arduino also.
   # program stopping condition
   if cv2.waitKey(1) & 0xFF == ord('q'):
     break
